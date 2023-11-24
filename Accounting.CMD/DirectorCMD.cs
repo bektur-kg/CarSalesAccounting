d13bdc2f02602ffe3c0d92088e32030ee9fe6bb8
bektur-kg <roadtothedreambeka81@gmail.com>
2023-11-24 13:53:12 +0600

    changed text

--- Accounting.CMD/DirectorCMD.cs
+++ Accounting.CMD/DirectorCMD.cs
@@ -119,7 +119,7 @@ namespace Accounting.CMD
             }
             else
             {
-                ConsoleOutput.WarningMessage("The list of sold cars is empty");
+                ConsoleOutput.WarningMessage("The list of sold cars is empty.");
             }
         }
 
